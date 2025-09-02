85bfc96c4034f329875cf2eef6f32fcf8865692f
[dyad] <frontdesk@hadleyresortandmarina.com>
2025-09-01 23:51:02 -0400

    [dyad] wrote 1 file(s), deleted 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./globals.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './app/globals.css'; // Changed import to src/app/globals.css
+import { SessionContextProvider } from './context/SessionContext.tsx';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SessionContextProvider>
+      <App />
+    </SessionContextProvider>
+  </React.StrictMode>,
+);
